faae1adfca3abdaef00d164c5ae1ec955277c85c
abhidjango-py <abhishekmishra45658@gmail.com>
2024-02-15 05:11:49 +0530

    Feature section completed

--- constants/index.ts
+++ constants/index.ts
@@ -20,7 +20,6 @@ export const FEATURES = [
   {
     title: "Real maps can be offline",
     icon: "/map.svg",
-    variant: "green",
     description:
       "We provide a solution for you to be able to use our application when climbing, yes offline maps you can use at any time there is no signal at the location",
   },
